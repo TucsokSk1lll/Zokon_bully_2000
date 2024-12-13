259575bc5f415c6475103abad8d4194f2ce76eee
Tucsok <jgyorgy008@gmail.com>
2024-12-13 17:19:24 +0100

    added visionscore

--- api2.py
+++ api2.py
@@ -9,9 +9,10 @@ def Find_info_about_zokon(game_id,game_index):
     Zokon_kills_and_deaths = {'Kill':None,'Death':None}
     Zokon_and_opponent_gold = {'Zokon':None,'Zokon_opponent':None}
     Zokon_and_opponent_level = {'Zokon':None,'Zokon_opponent':None}
+    Zokon_vision = {'Zokon':None}
     
     Infos = {'game_index':game_index,'gameid':game_id,'Zokon_and_opponent_champ':Zokon_and_opponent_champ,'Zokon_kills_and_deaths':Zokon_kills_and_deaths,'Zokon_and_opponent_gold':Zokon_and_opponent_gold,
-             'Zokon_and_opponent_level':Zokon_and_opponent_level}
+             'Zokon_and_opponent_level':Zokon_and_opponent_level,'Zokon_vision':Zokon_vision}
     
     Zokon_lane = None
     zokon_team_id = None
@@ -20,7 +21,7 @@ def Find_info_about_zokon(game_id,game_index):
     
     def Zokon_game_info():
         nonlocal Zokon_lane, zokon_team_id
-        if game['info']['gameMode'] != 'ARAM':
+        if game['info']['gameMode'] == 'CLASSIC':
             for i in range(10):
                 if game['info']['participants'][i]['puuid'] == Zokon_puuid:
                     Zokon_and_opponent_id['Zokon'] = i
@@ -38,6 +39,7 @@ def Find_info_about_zokon(game_id,game_index):
                     Zokon_and_opponent_gold['Zokon_opponent'] = game['info']['participants'][Zokon_and_opponent_id['Zokon_opponent']]['goldEarned']
                     Zokon_and_opponent_level['Zokon'] = game['info']['participants'][Zokon_and_opponent_id['Zokon']]['champLevel']
                     Zokon_and_opponent_level['Zokon_opponent'] = game['info']['participants'][Zokon_and_opponent_id['Zokon_opponent']]['champLevel']
+                    Zokon_vision['Zokon'] = game['info']['participants'][Zokon_and_opponent_id['Zokon']]['visionScore']
                     break
                 
                 
@@ -46,10 +48,10 @@ def Find_info_about_zokon(game_id,game_index):
         #return Zokon_and_opponent_champ,Zokon_kills_and_deaths,Zokon_and_opponent_gold,Zokon_and_opponent_level
         return Infos
 def Zokon_bully():
-    number_of_matches = 10
+    number_of_matches = 100
     try:
         games = requests.get(f'https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{Zokon_puuid}/ids?start=0&count={number_of_matches}&api_key={api_key.api_key}').json()
-        print(games)
+        #print(games)
         
         lst_zokon_and_lane_opponent = []
         for i in range(number_of_matches):
